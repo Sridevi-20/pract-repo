9d51fcdb1944346b3b082a72e40acee94c7d000b
Sridevi Ungarala <srideviungarala@Sridevis-MBP.hsd1.wa.comcast.net>
2023-01-18 11:44:21 -0800

    This is quartzscheduleremail project

---
+++ src/test/java/com/example/quartzemailscheduler/QuartzEmailSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartzemailscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzEmailSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
